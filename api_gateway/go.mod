1cc64f5a69bb24276dfba25154e75dc699360e91
Christoffer Lundell <christofferlundell@protonmail.com>
2025-04-08 15:57:30 +0200

    add authentication endpoint

--- api_gateway/go.mod
+++ api_gateway/go.mod
@@ -5,3 +5,5 @@ go 1.23.0
 toolchain go1.24.2
 
 require golang.org/x/time v0.11.0
+
+require github.com/google/uuid v1.6.0 // indirect
--- api_gateway/main.go
+++ api_gateway/main.go
@@ -1,64 +1,114 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "net/http/httputil"
-    "net/url"
-    "sync"
-    "time"
-    "net"
+	"html/template"
+	"log"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"sync"
+	"time"
 
-    "golang.org/x/time/rate"
+	"github.com/google/uuid"
+	"golang.org/x/time/rate"
 )
 
 var mu sync.Mutex
 var clientLimits = make(map[string]*rate.Limiter)
 
 func getLimiter(clientIP string) *rate.Limiter {
-    mu.Lock()
-    defer mu.Unlock()
+	mu.Lock()
+	defer mu.Unlock()
 
-    limiter, exists := clientLimits[clientIP]
-    if !exists {
-        limiter = rate.NewLimiter(rate.Every(time.Second*1), 2)
-        clientLimits[clientIP] = limiter
-    }
-    return limiter
+	limiter, exists := clientLimits[clientIP]
+	if !exists {
+		limiter = rate.NewLimiter(rate.Every(time.Second*1), 2)
+		clientLimits[clientIP] = limiter
+	}
+	return limiter
 }
 
+var authTemplate = `
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Sign in</title>
+</head>
+<body>
+	<h1>Sign in</h1>
+	<form method="POST" action="/auth">
+		<input type="text" name="token">
+		<br><br>
+		<input type="submit" value="Authenticate">
+	</form>
+</body>
+</html>
+`
+
+// TODO: add which endpoints the token is authorized for
+var authorizedTokens map[string]bool = map[string]bool{}
+
 func main() {
-    // Define the backend service URL
-    backendURL, err := url.Parse("http://backend-service:8081")
-    if err != nil {
-        panic(err)
-    }
+	// Define the backend service URL
+	backendURL, err := url.Parse("http://backend-service:8081")
+	if err != nil {
+		panic(err)
+	}
 
-    redirect := httputil.NewSingleHostReverseProxy(backendURL)
+	backendRedirect := httputil.NewSingleHostReverseProxy(backendURL)
+	// Request handler for the API Gateway
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		clientIP, _, _ := net.SplitHostPort(r.RemoteAddr)
+		log.Printf("Request from %s: %s", clientIP, r.URL.Path)
 
-    // Request handler for the API Gateway
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	clientIP,_,_ := net.SplitHostPort(r.RemoteAddr)
-	log.Printf("Request from %s: %s", clientIP, r.URL.Path)
+		limiter := getLimiter(clientIP)
+		allowed := limiter.Allow()
+		if !limiter.Allow() {
+			log.Printf("Rate limit exceeded by %s", clientIP)
+			http.Error(w, "Rate limit exceeded", http.StatusTooManyRequests)
+			return
+		}
+		log.Printf("limiter allowed for %s: %v", clientIP, allowed)
 
-        limiter := getLimiter(clientIP)
-	allowed := limiter.Allow()
-        if !limiter.Allow() {
-	    log.Printf("Rate limit exceeded by %s", clientIP)
-            http.Error(w, "Rate limit exceeded", http.StatusTooManyRequests)
-            return
-        }
-	log.Printf("limiter allowed for %s: %v", clientIP, allowed)
+		if r.URL.Path == "/" {
+			proxyAuthCookie, err := r.Cookie("ProxyAuth")
+			if err != nil {
+				http.Error(w, "no such cookie ProxyAuth: "+err.Error(), http.StatusInternalServerError)
+				return
+			}
+			_, ok := authorizedTokens[proxyAuthCookie.Value]
+			if ok {
+				backendRedirect.ServeHTTP(w, r)
+			} else {
+				http.Error(w, "bad token", http.StatusBadRequest)
+			}
+		} else if r.URL.Path == "/auth" {
+			if r.Method == "GET" {
+				t := template.Must(template.New("auth").Parse(authTemplate))
+				err = t.Execute(w, nil)
+				if err != nil {
+					log.Println("Failee at executing form template" + err.Error())
+				}
+			} else if r.Method == "POST" {
+				log.Println(r.Body)
+				if r.FormValue("token") == "1234" {
+					var token string = uuid.NewString()
+					authorizedTokens[token] = true
+					w.Header().Set("Set-Cookie", "ProxyAuth="+token)
+					http.Redirect(w, r, "/", http.StatusFound)
+				} else {
+					http.Error(w, "invalid credentials", http.StatusBadRequest)
+				}
+			}
+		} else {
+			log.Printf("Invalid request from %s: %s", clientIP, r.URL.Path)
+			http.Error(w, "Invalid request", http.StatusBadRequest)
+		}
+	})
 
-	if (r.URL.Path == "/getAuthRequests" || r.URL.Path == "/auth") && r.Header.Get("authentication") == "true" {
-            redirect.ServeHTTP(w, r)
-        } else {
-	    log.Printf("Invalid request from %s: %s", clientIP, r.URL.Path)
-            http.Error(w, "Invalid request", http.StatusBadRequest)
-        }
-    })
-
-    // Start API Gateway server
-    log.Println("Starting API Gateway on :8080")
-    http.ListenAndServe(":8080", nil)
+	// Start API Gateway server
+	log.Println("Starting API Gateway on :8080")
+	http.ListenAndServe(":8080", nil)
 }
